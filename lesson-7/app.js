94c259caf11d6d0f4b067b8db6c0ac4bea636989
sayakanoa <89767987+sayakanoa@users.noreply.github.com>
2022-01-16 20:35:35 +0900

    childrenを使用

--- lesson-7/app.js
+++ lesson-7/app.js
@@ -1,17 +1,5 @@
 $(function(){
-  $(".bg1").on("click", function(){
-    $(this).slideUp();
-  });
-
-  $(".bg2").on("click", function(){
-    $(this).slideUp();
-  });
-
-  $(".bg3").on("click", function(){
-    $(this).slideUp();
-  });
-
-  $(".bg4").on("click", function(){
-    $(this).slideUp();
+  $("button").on("click", function(){
+    $("ul").children().css("color","red");
   });
 });
