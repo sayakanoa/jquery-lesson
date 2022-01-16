074b28f00c75b9b87e6509d458e3893091e40d4d
sayakanoa <89767987+sayakanoa@users.noreply.github.com>
2022-01-16 20:29:48 +0900

    thisを使ってアニメーション

---
+++ lesson-7/app.js
@@ -0,0 +1,17 @@
+$(function(){
+  $(".bg1").on("click", function(){
+    $(this).slideUp();
+  });
+
+  $(".bg2").on("click", function(){
+    $(this).slideUp();
+  });
+
+  $(".bg3").on("click", function(){
+    $(this).slideUp();
+  });
+
+  $(".bg4").on("click", function(){
+    $(this).slideUp();
+  });
+});
